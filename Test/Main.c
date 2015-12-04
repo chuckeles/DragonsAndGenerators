cf6ec38132d36d8c53df0f1e5dffe748f3fba16e
Martin Chuckeles <martin@jakubik.info>
2015-12-05 00:12:47 +0100

    Added a primitive test

--- Test/Main.c
+++ Test/Main.c
@@ -5,6 +5,25 @@
  */
 
 #include <check.h>
+#include <stdlib.h>
+
+#include "History.h"
+
+/*
+ * Test for the History enum.
+ */
+START_TEST(HistoryTest)
+{
+
+  // check the empty history
+  ck_assert_int_eq(HistoryEmpty, 0);
+
+  // check the full history
+  ck_assert_int_eq(HistoryAll,
+                   HistoryDragon | HistoryGenerator | HistoryPrincess0 | HistoryPrincess1 | HistoryPrincess2);
+
+}
+END_TEST
 
 /*
  * Testing entry point.
