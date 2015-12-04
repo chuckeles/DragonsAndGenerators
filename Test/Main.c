175d92c2abb78b7294030402d0be50c0ea715330
Martin Chuckeles <martin@jakubik.info>
2015-12-04 23:58:43 +0100

    Changed History.c to Main.c

---
+++ Test/Main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) Chuckeles
+ *
+ * For license information, check the LICENSE.md file.
+ */
+
+#include <check.h>
+
+/*
+ * Testing entry point.
+ */
+int main() {
+
+  return 0;
+
+}
