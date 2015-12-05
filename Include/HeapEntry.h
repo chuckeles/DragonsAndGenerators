8d80fa8522d4cf5792eb3bf0b5220a72d1c805df
Martin Chuckeles <martin@jakubik.info>
2015-12-05 12:22:27 +0100

    Added heap entry

---
+++ Include/HeapEntry.h
@@ -0,0 +1,34 @@
+#ifndef _HEAPENTRY_H
+#define _HEAPENTRY_H
+
+#include "BaseTypes.h"
+#include "History.h"
+
+/*
+ * A single entry in the processing minimum binary heap.
+ */
+typedef struct HeapEntry {
+
+  /*
+   * Path length up to this tile, including the tile.
+   */
+  uchar path;
+
+  /*
+   * Direction to this tile.
+   */
+  uchar direction;
+
+  /*
+   * Stage history.
+   */
+  History history;
+
+  /*
+   * Tile number in the stage.
+   */
+  uint tile;
+
+} HeapEntry;
+
+#endif
