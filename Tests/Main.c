e3cb37a6345f612e4d6b6581a7c055916df73756
Martin Chuckeles <martin@jakubik.info>
2015-12-05 01:15:05 +0100

    Wrote a test suite and all

--- Tests/Main.c
+++ Tests/Main.c
@@ -30,6 +30,24 @@ END_TEST
  */
 int main() {
 
-  return 0;
+  // create suite and test case
+  Suite* suite = suite_create("History");
+  TCase* tcase = tcase_create("Main");
+
+  // add test
+  tcase_add_test(tcase, HistoryTest);
+  suite_add_tcase(suite, tcase);
+
+  // create a runner
+  SRunner* srunner = srunner_create(suite);
+
+  // run tests
+  srunner_run_all(srunner, CK_NORMAL);
+  int failed = srunner_ntests_failed(srunner);
+
+  // free
+  srunner_free(srunner);
+
+  return failed > 0 ? EXIT_FAILURE : EXIT_SUCCESS;
 
 }
