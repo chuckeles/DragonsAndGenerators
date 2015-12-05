fa843eb0baffcdca5b846b74dec0cda54b3d516a
Martin Chuckeles <martin@jakubik.info>
2015-12-05 23:28:58 +0100

    Added dummy declarations

---
+++ Source/Stage.c
@@ -0,0 +1,15 @@
+#include "Stage.h"
+
+/*
+ * Definitions for stage functions.
+ */
+
+Stage* CreateStages(char* map, ushort width, ushort height) {
+
+  return NULL;
+
+}
+
+void DeleteStages(Stage* stages) {
+
+}
