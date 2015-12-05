ef46c214a36300a46f70d5effed14241189678bb
Martin Chuckeles <martin@jakubik.info>
2015-12-05 18:22:00 +0100

    Added Stage.h

---
+++ Include/Stage.h
@@ -0,0 +1,34 @@
+#ifndef _STAGE_H
+#define _STAGE_H
+
+#include "Base.h"
+#include "History.h"
+
+/*
+ * A single stage represented by a map, path lengths and directions.
+ */
+typedef struct Stage {
+
+  /*
+   * The history of the stage.
+   */
+  History history;
+
+  /*
+   * The map tiles.
+   */
+  char* tiles;
+
+  /*
+   * Tile paths.
+   */
+  uchar* paths;
+
+  /*
+   * Directions to previous tile.
+   */
+  uchar* directions;
+
+} Stage;
+
+#endif
