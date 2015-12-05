bcbdb6a72cf6a75a661d26c806fa320f2c7ed0b5
Martin Chuckeles <martin@jakubik.info>
2015-12-05 12:01:03 +0100

    Added BaseTypes.h

---
+++ Include/BaseTypes.h
@@ -0,0 +1,12 @@
+#ifndef _BASETYPES_H
+#define _BASETYPES_H
+
+/*
+ * Base types shortcutted for better usage.
+ */
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+#endif
