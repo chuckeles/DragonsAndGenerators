4b746c5920adf9a0f5ebeab66fc9db70fecfb497
Martin Chuckeles <martin@jakubik.info>
2015-12-05 23:52:46 +0100

    Initializing the first stage

--- Source/Stage.c
+++ Source/Stage.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "History.h"
 #include "Stage.h"
@@ -9,8 +10,32 @@
 
 Stage* CreateStages(char* map, ushort width, ushort height) {
 
+  // bloody iterator
+  uint i;
+
   // allocate the stage array
-  return calloc(HistoryAll - 1, sizeof(Stage));
+  Stage* stages = calloc(HistoryAll - 1, sizeof(Stage));
+
+  // initialize the first stage's tiles
+  stages[0].tiles = strdup(map);
+
+  // make the paths and directions
+  uint size = width * height;
+  stages[0].paths = malloc(size * sizeof(char));
+  stages[0].directions = malloc(size * sizeof(char));
+
+  for (i = 0; i < size; ++i) {
+    stages[0].paths[i] = 0;
+    stages[0].directions[i] = 0;
+  }
+
+  // initialize history
+  for (i = HistoryEmpty; i < HistoryAll; ++i) {
+    stages[i].history = (History) i;
+  }
+
+  // return the stages
+  return stages;
 
 }
 
