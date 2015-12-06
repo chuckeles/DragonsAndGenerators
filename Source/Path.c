44ebd40fd5ae6a1266a701c20eefd13592914fa2
Martin Chuckeles <martin@jakubik.info>
2015-12-06 11:36:04 +0100

    Preparation for the Dijkstra's algorithm

--- Source/Path.c
+++ Source/Path.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 
+#include "Heap.h"
 #include "Path.h"
 
 /*
@@ -7,6 +8,27 @@
  */
 
 int* FindPath(Stage* stages, ushort width, ushort height, uint* length) {
+
+  // create a heap
+  Heap heap = CreateHeap();
+
+  // make the first tile
+  HeapEntry firstTile;
+  firstTile.path = 1;
+  firstTile.direction = 0;
+  firstTile.history = HistoryEmpty;
+  firstTile.tile = 0;
+
+  // push it to the heap
+  HeapPush(&heap, firstTile);
+
+  // loop until we find the path or run out of tiles
+  while (heap.size > 0) {
+    
+  }
+
+  // delete the heap
+  DeleteHeap(heap);
 
   return NULL;
 
