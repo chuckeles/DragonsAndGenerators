2e4e035d8821df3e9b6627aef2f50c85520105dc
Martin Chuckeles <martin@jakubik.info>
2015-12-06 18:16:36 +0100

    Moving to a dragon stage

--- Source/Path.c
+++ Source/Path.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "Heap.h"
 #include "Path.h"
@@ -24,8 +25,74 @@
     neighbour.history = entry.history; \
     neighbour.tile = tile; \
     \
-    HeapPush(&heap, neighbour); \
+    HeapPush(heap, neighbour); \
   }
+
+/*
+ * Processes a tile. Updates it and pushes the neighbours.
+ */
+inline void ProcessTile(Heap* heap, Stage* stage, HeapEntry entry, ushort x, ushort y, ushort width, ushort height) {
+
+  // update the tile
+  stage->paths[entry.tile] = entry.path;
+  stage->directions[entry.tile] = entry.direction;
+
+  // add neighbours to the heap
+  HeapEntry neighbour;
+  ushort neighbourX;
+  ushort neighbourY;
+
+  // left
+  if (x > 0) {
+    // set position
+    neighbourX = (ushort) (x - 1);
+    neighbourY = y;
+
+    PUSH_NEIGHBOUR(0)
+  }
+
+  // up
+  if (y > 0) {
+    // set position
+    neighbourX = x;
+    neighbourY = (ushort) (y - 1);
+
+    PUSH_NEIGHBOUR(1)
+  }
+
+  // down
+  if (y < height - 1) {
+    // set position
+    neighbourX = x;
+    neighbourY = (ushort) (y + 1);
+
+    PUSH_NEIGHBOUR(2)
+  }
+
+  // right
+  if (x < width - 1) {
+    // set position
+    neighbourX = (ushort) (x + 1);
+    neighbourY = y;
+
+    PUSH_NEIGHBOUR(3)
+  }
+
+}
+
+/*
+ * Creates the new stage if it doesn't exist yet.
+ */
+inline void EnsureStageExists(Stage* stage, Stage* newStage, ushort width, ushort height) {
+
+  // check if it doesn't exist
+  if (newStage->tiles == NULL) {
+    newStage->tiles = strdup(stage->tiles);
+    newStage->paths = calloc(width * height, sizeof(uchar));
+    newStage->directions = malloc(width * height * sizeof(uchar));
+  }
+
+}
 
 /*
  * Definitions for path-finding functions.
@@ -68,52 +135,37 @@ int* FindPath(Stage* stages, ushort width, ushort height, uint* length) {
 
       // check the tile
       switch (stage->tiles[entry.tile]) {
+        case 'd':
+          // check if already killed dragon
+          if (entry.history & HistoryDragon) {
+            // just process
+            ProcessTile(&heap, stage, entry, x, y, width, height);
+          }
+          else {
+            // update the tile
+            stage->paths[entry.tile] = entry.path;
+            stage->directions[entry.tile] = entry.direction;
+
+            // update history
+            entry.history &= HistoryDragon;
+
+            // get the new stage
+            Stage* newStage = stages + entry.history;
+
+            // ensure the stage exists
+            EnsureStageExists(stage, newStage, width, height);
+
+            // get tile's existing path
+            uchar oldPath2 = stage->paths[entry.tile];
+
+            // check the existing path
+            if (oldPath2 == 0 || oldPath2 > entry.path) {
+              ProcessTile(&heap, newStage, entry, x, y, width, height);
+            }
+          }
+          break;
         default:
-          // update the tile
-          stage->paths[entry.tile] = entry.path;
-          stage->directions[entry.tile] = entry.direction;
-
-          // add neighbours to the heap
-          HeapEntry neighbour;
-          ushort neighbourX;
-          ushort neighbourY;
-
-          // left
-          if (x > 0) {
-            // set position
-            neighbourX = (ushort) (x - 1);
-            neighbourY = y;
-
-            PUSH_NEIGHBOUR(0)
-          }
-
-          // up
-          if (y > 0) {
-            // set position
-            neighbourX = x;
-            neighbourY = (ushort) (y - 1);
-
-            PUSH_NEIGHBOUR(1)
-          }
-
-          // down
-          if (y < height - 1) {
-            // set position
-            neighbourX = x;
-            neighbourY = (ushort) (y + 1);
-
-            PUSH_NEIGHBOUR(2)
-          }
-
-          // right
-          if (x < width - 1) {
-            // set position
-            neighbourX = (ushort) (x + 1);
-            neighbourY = y;
-
-            PUSH_NEIGHBOUR(3)
-          }
-
+          ProcessTile(&heap, stage, entry, x, y, width, height);
           break;
       }
     }
