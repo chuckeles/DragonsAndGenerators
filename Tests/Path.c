b9448f18999ae0799e168cfcb144c6d6b602bae7
Martin Chuckeles <martin@jakubik.info>
2015-12-06 11:18:50 +0100

    Added simple path-finding test

--- Tests/Path.c
+++ Tests/Path.c
@@ -1,4 +1,32 @@
+#include "Stage.h"
 #include "Tests.h"
+
+/*
+ * Simple path-finding test without a generator.
+ */
+START_TEST(SimplePathTest)
+{
+
+  // make stages
+  Stage* stages = CreateStages("CDPPP", 5, 1);
+
+  // get the path
+  uint length;
+  int* path = FindPath(stages, 5, 1);
+
+  // check the path
+  ushort i;
+  for (i = 0; i < 5; ++i) {
+    ck_assert_int_eq(path[i * 2], i);
+    ck_assert_int_eq(path[i * 2 + 1], 0);
+  }
+
+  // delete path and stages
+  DeletePath(path);
+  DeleteStages(stages);
+
+}
+END_TEST
 
 /*
  * Path-finding test case.
@@ -9,6 +37,7 @@ TCase* PathTCase() {
   TCase* tcase = tcase_create("Path");
 
   // add tests
+  tcase_add_test(tcase, SimplePathTest);
 
   // return the test case
   return tcase;
