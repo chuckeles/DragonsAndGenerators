7af07de3f087606c6c41b3cad0087144730d2e73
Martin Chuckeles <martin@jakubik.info>
2015-12-06 12:32:04 +0100

    Changed simple path test to just check paths in the first stage

--- Tests/Path.c
+++ Tests/Path.c
@@ -3,26 +3,22 @@
 #include "Tests.h"
 
 /*
- * Simple path-finding test without a generator.
+ * Simple path-finding test.
  */
 START_TEST(SimplePathTest)
 {
 
   // make stages
-  Stage* stages = CreateStages("cdpqr", 5, 1);
+  Stage* stages = CreateStages("ccccc", 5, 1);
 
   // get the path
   uint length;
   int* path = FindPath(stages, 5, 1, &length);
 
-  // check the length
-  ck_assert_int_eq(length, 5);
-
-  // check the path
+  // check the path in the first stage
   ushort i;
   for (i = 0; i < 5; ++i) {
-    ck_assert_int_eq(path[i * 2], i);
-    ck_assert_int_eq(path[i * 2 + 1], 0);
+    ck_assert_int_eq(stages[HistoryEmpty].paths[i], i + 1);
   }
 
   // delete path and stages
