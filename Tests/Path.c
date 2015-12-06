9ecf68983438641e14cd7d3b29410776d48dbde4
Martin Chuckeles <martin@jakubik.info>
2015-12-06 12:34:25 +0100

    Added simple dragon path-finding test

--- Tests/Path.c
+++ Tests/Path.c
@@ -29,6 +29,39 @@ START_TEST(SimplePathTest)
 END_TEST
 
 /*
+ * Simple path-finding test with a dragon.
+ */
+START_TEST(SimplePathDragonTest)
+{
+
+  // bloody iterator
+  ushort i;
+
+  // make stages
+  Stage* stages = CreateStages("ccdcc", 5, 1);
+
+  // get the path
+  uint length;
+  int* path = FindPath(stages, 5, 1, &length);
+
+  // check the path in the first stage
+  for (i = 0; i < 3; ++i) {
+    ck_assert_int_eq(stages[HistoryEmpty].paths[i], i + 1);
+  }
+
+  // check the path in the dragon stage
+  for (i = 2; i < 5; ++i) {
+    ck_assert_int_eq(stages[HistoryDragon].paths[i], i + 1);
+  }
+
+  // delete path and stages
+  DeletePath(path);
+  DeleteStages(stages);
+
+}
+END_TEST
+
+/*
  * Path-finding test case.
  */
 TCase* PathTCase() {
@@ -38,6 +71,7 @@ TCase* PathTCase() {
 
   // add tests
   tcase_add_test(tcase, SimplePathTest);
+  tcase_add_test(tcase, SimplePathDragonTest);
 
   // return the test case
   return tcase;
