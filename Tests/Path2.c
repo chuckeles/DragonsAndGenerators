f7221a04702bcd1366e6e3c5e0a30eb9dde26e8c
Martin Chuckeles <martin@jakubik.info>
2015-12-07 22:55:01 +0100

    Added test for checking lexicographical order

--- Tests/Path2.c
+++ Tests/Path2.c
@@ -67,6 +67,43 @@ START_TEST(PathResultLongerTest)
 END_TEST
 
 /*
+ * Path-finding test with a simple resulting path.
+ */
+START_TEST(PathDirectionTest)
+{
+
+  // create stages
+  Stage* stages = CreateStages("ccccdpcqr", 3, 3);
+
+  // find the path
+  uint length;
+  int* path = FindPath(stages, 3, 3, &length);
+
+  // check the length
+  ck_assert_int_eq(length, 6);
+
+  // check the path
+  ck_assert_int_eq(path[0], 0);
+  ck_assert_int_eq(path[1], 0);
+  ck_assert_int_eq(path[2], 0);
+  ck_assert_int_eq(path[3], 1);
+  ck_assert_int_eq(path[4], 1);
+  ck_assert_int_eq(path[5], 1);
+  ck_assert_int_eq(path[6], 1);
+  ck_assert_int_eq(path[7], 2);
+  ck_assert_int_eq(path[8], 2);
+  ck_assert_int_eq(path[9], 2);
+  ck_assert_int_eq(path[10], 2);
+  ck_assert_int_eq(path[11], 1);
+
+  // delete path and stages
+  DeletePath(path);
+  DeleteStages(stages);
+
+}
+END_TEST
+
+/*
  * Path-finding test case.
  */
 TCase* Path2TCase() {
@@ -77,6 +114,7 @@ TCase* Path2TCase() {
   // add tests
   tcase_add_test(tcase, PathResultTest);
   tcase_add_test(tcase, PathResultLongerTest);
+  tcase_add_test(tcase, PathDirectionTest);
 
   // return the test case
   return tcase;
