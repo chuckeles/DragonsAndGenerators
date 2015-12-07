6915508eac9758adba5b16da692aafe6efe6c7de
Martin Chuckeles <martin@jakubik.info>
2015-12-07 21:30:42 +0100

    Added longer path test

--- Tests/Path2.c
+++ Tests/Path2.c
@@ -32,6 +32,41 @@ START_TEST(PathResultTest)
 END_TEST
 
 /*
+ * Path-finding test with a simple resulting path.
+ */
+START_TEST(PathResultLongerTest)
+{
+
+  // create stages
+  Stage* stages = CreateStages("cdcprq", 2, 3);
+
+  // find the path
+  uint length;
+  int* path = FindPath(stages, 2, 3, &length);
+
+  // check the length
+  ck_assert_int_eq(length, 5);
+
+  // check the path
+  ck_assert_int_eq(path[0], 0);
+  ck_assert_int_eq(path[1], 0);
+  ck_assert_int_eq(path[2], 1);
+  ck_assert_int_eq(path[3], 0);
+  ck_assert_int_eq(path[4], 1);
+  ck_assert_int_eq(path[5], 1);
+  ck_assert_int_eq(path[6], 1);
+  ck_assert_int_eq(path[7], 2);
+  ck_assert_int_eq(path[8], 0);
+  ck_assert_int_eq(path[9], 2);
+
+  // delete path and stages
+  DeletePath(path);
+  DeleteStages(stages);
+
+}
+END_TEST
+
+/*
  * Path-finding test case.
  */
 TCase* Path2TCase() {
@@ -41,6 +76,7 @@ TCase* Path2TCase() {
 
   // add tests
   tcase_add_test(tcase, PathResultTest);
+  tcase_add_test(tcase, PathResultLongerTest);
 
   // return the test case
   return tcase;
