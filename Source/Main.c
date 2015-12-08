922933edea0695b1d4863fe51342e9730e74cf48
Martin Chuckeles <martin@jakubik.info>
2015-12-08 17:06:50 +0100

    Wrote the assignment function

--- Source/Main.c
+++ Source/Main.c
@@ -4,15 +4,37 @@
  * For license information, check the LICENSE.md file.
  */
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Path.h"
 
 /*
- * Program entry point.
+ * Kills the dragon and finds the princesses. Keeps it sweet and short.
+ * This is according to the assignment.
  */
-int main() {
+int* zachran_princezne(char** mapa, int n, int m, int t, int* dlzka_cesty) {
 
-  printf("Hello World\n");
+  // make an array for the map
+  char* mapArray = malloc((n * m + 1) * sizeof(char));
 
-  return 0;
+  // copy the map
+  uint i;
+  for (i = 0; i < n; ++i) {
+    strcpy(mapArray + i * m, mapa[i]);
+  }
+
+  // create the stages
+  Stage* stages = CreateStages(mapArray, (ushort) m, (ushort) n);
+
+  // get the path
+  int* path = FindPath(stages, (ushort) m, (ushort) n, (uint*) dlzka_cesty);
+
+  // delete all the stuff
+  DeleteStages(stages);
+  free(mapArray);
+
+  // return the result
+  return path;
 
 }
